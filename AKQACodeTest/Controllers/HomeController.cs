664499b0d8d4ce9cf5d1395e57dcb0f537c570e2
AHUJA <chinkey.ahuja@agilent.com>
2020-03-17 18:08:25 +0530

    First Commit

---
+++ AKQACodeTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace AKQACodeTest.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UserEntry()
+        {
+         
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UserEntry(FormCollection formCollection, string command)
+        {
+            ValuesController obj = new ValuesController();
+
+            ViewBag.Title = formCollection["txtUserName"].ToUpper();
+            ViewBag.Message = obj.Get(formCollection["txtNumber"]).ToUpper();
+            return View();
+        }
+    }
+}
---
+++ AKQACodeTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AKQACodeTest.WebApiApplication" Language="C#" %>
